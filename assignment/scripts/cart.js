7d8c894cc2d278d01bcbe3ecef6fba94d705601b
Theo Janke-Furman <tjankefurman@gmail.com>
2022-08-01 23:15:39 -0500

    Completed required and stretch goals in cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,61 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+    if (isFull(basket)) {
+        console.log("Your basket is full!");
+        return false;
+    } else if (!isFull(basket)) {
+        basket.unshift(item);
+        if (basket[0] === item) {
+            console.log(`New item: ${basket[0]}`)
+            console.log(`Basket is now ${basket}`)
+            return true;
+        } else {
+            console.log("Something went wrong")
+            return false;
+        }
+    }
+}
+console.log(addItem("pears"), addItem("broccoli"), addItem("asparagus"))
+
+function listItems() {
+    for (let i = 0; i < basket.length; i++) {
+        console.log(`${basket[i]}\n`)
+    }
+}
+console.log(listItems());
+
+function empty(array) {
+    array = [];
+    return array;
+}
+console.log(empty(basket), `Basket is now ${basket}`);
+
+const maxItems = 5;
+
+function isFull(array) {
+    if (array.length < maxItems) {
+        return false;
+    } else if (array.length >= maxItems) {
+        return true;
+    }
+}
+
+console.log(addItem("apples"), addItem("cherries"), addItem("beats"), addItem("peppers"), addItem("strawberries"));
+isFull(basket) ? console.log("Your basket is full!") : console.log("There's still room for more");
+
+function removeItem(item) {
+    let itemPlacement = basket.indexOf(item);
+    if (itemPlacement === -1) {
+        return null;
+    } else {
+        basket.splice(itemPlacement, 1);
+        return item;
+    }
+}
+
+console.log(`${removeItem(beats)} was removed. Basket now contains ${basket}`);
