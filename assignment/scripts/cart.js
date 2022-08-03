00da4c5bec55dcef046671dce9565a6483c37021
Theo Janke-Furman <tjankefurman@gmail.com>
2022-08-03 16:57:35 -0500

    Minor console.log edits in cart.js

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -9,6 +9,7 @@ const maxItems = 5;
 
 function addItem(item) {
     if (isFull(basket)) {
+        console.log(`Tried to add: ${item}`)
         console.log("Your basket is full!");
         return false;
     } else if (!isFull(basket)) {
@@ -23,7 +24,8 @@ function addItem(item) {
         }
     }
 }
-console.log(addItem("pears"), addItem("broccoli"), addItem("asparagus"))
+// Console log necessary for 'true'/'false' to be logged?
+console.log(addItem("pears"), addItem("broccoli"), addItem("asparagus"));
 
 function listItems() {
     for (let i = 0; i < basket.length; i++) {
@@ -50,8 +52,7 @@ function isFull(array) {
     }
 }
 
-console.log(addItem("apples"), addItem("cherries"), addItem("beats"), addItem("peppers"), addItem("strawberries"));
-isFull(basket) ? console.log("Your basket is full!") : console.log("There's still room for more");
+console.log(addItem("apples"), addItem("cherries"), addItem("beats"), addItem("peppers"), addItem("strawberries"), addItem("grapes"));
 
 function removeItem(item) {
     let itemPlacement = basket.indexOf(item);
@@ -64,3 +65,4 @@ function removeItem(item) {
 }
 
 console.log(`${removeItem("beats")} was removed. Basket now contains ${basket.join(", ")}`);
+isFull(basket) ? console.log("Your basket is full!") : console.log("There's room for more!");
